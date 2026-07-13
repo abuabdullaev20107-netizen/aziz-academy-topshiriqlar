e9b9982f892359ab832a7800e05d3202c6a1af54
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-13 12:24:55 +0500

    Add: 12) print end parametri [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/03-Birinchi_dastur_____print___va_kommentlar/12-12__print_end_parametri.py
@@ -0,0 +1,5 @@
+# Ikki marta print chaqirib,
+# konsolga quyidagini chiqarib bering (hammasi bitta qatorda):
+# Salom!
+# Masalan: "Salom!" so'zi va oxirida yangi qatordan foydalanmang.
+print("Salom!")
