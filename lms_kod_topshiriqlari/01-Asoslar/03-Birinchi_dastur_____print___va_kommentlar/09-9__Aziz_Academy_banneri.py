124c4ccb074a4783862563e80aeb60c6890149bc
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-13 12:23:29 +0500

    Add: 9) Aziz Academy banneri [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/03-Birinchi_dastur_____print___va_kommentlar/09-9__Aziz_Academy_banneri.py
@@ -0,0 +1,7 @@
+# print() yordamida quyidagi banner matnini chiqaring:
+# Aziz Academy
+# Python kursi
+# Birinchi modul: Kirish
+print("Aziz Academy")
+print("Python kursi")
+print("Birinchi modul: Kirish")
