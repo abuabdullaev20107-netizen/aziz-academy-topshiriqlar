1416ee45e924a1a9c89455194c96b8f7f52d96d6
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-13 12:12:14 +0500

    Add: 3) Bir nechta qatorni bitta print bilan chiqarish [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/03-Birinchi_dastur_____print___va_kommentlar/03-3__Bir_nechta_qatorni_bitta_print_bilan_chiqarish.py
@@ -0,0 +1,6 @@
+# Quyidagi matnni CONSOLGA CHIQARING (natija 3 qator bo‘ladi):
+# Bugungi reja:
+# 1. Python o'rnatish
+# 2. Birinchi dastur
+# Istasangiz \n escape belgisidan foydalanishingiz mumkin.
+print("Bugungi reja:\n1. Python o'rnatish\n2. Birinchi dastur")
