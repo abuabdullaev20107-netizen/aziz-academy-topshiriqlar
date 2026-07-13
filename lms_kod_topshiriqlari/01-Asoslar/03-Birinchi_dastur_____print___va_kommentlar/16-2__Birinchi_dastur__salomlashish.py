42a86eb8eb7391e15b2d101a595561ca4f45c9d9
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-13 12:31:45 +0500

    Add: 2) Birinchi dastur: salomlashish [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/03-Birinchi_dastur_____print___va_kommentlar/16-2__Birinchi_dastur__salomlashish.py
@@ -0,0 +1,5 @@
+# Konsolga ikki qatorda matn chiqaring:
+# 1-qator: Assalomu alaykum
+# 2-qator: Bu mening birinchi Python dasturim
+print("Assalomu alaykum")
+print("Bu mening birinchi Python dasturim")
