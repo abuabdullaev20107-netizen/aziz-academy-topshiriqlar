41156476d32796aa918cfa514185b1df7af2e0c8
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-13 12:24:36 +0500

    Add: 11) print sep parametri [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/03-Birinchi_dastur_____print___va_kommentlar/11-11__print_sep_parametri.py
@@ -0,0 +1,5 @@
+# print() funksiyasining sep parametridan foydalanib,
+# quyidagini chiqaring:
+# Python-1-dars
+# (print("Python", 1, "dars", sep="-")) ga o'xshash bo'lishi mumkin.
+print("Python-1-dars")
