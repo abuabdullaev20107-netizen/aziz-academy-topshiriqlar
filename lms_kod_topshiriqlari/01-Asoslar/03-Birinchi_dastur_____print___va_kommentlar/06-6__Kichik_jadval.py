467652354f00de8d1c3de0c56bf64284ec85bc34
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-13 12:18:56 +0500

    Add: 6) Kichik jadval [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/03-Birinchi_dastur_____print___va_kommentlar/06-6__Kichik_jadval.py
@@ -0,0 +1,9 @@
+# Konsolga quyidagicha kichik jadval chiqaring:
+# Dars jadvali
+# Dushanba
+# Seshanba
+# Chorshanba
+print("Dars jadvali")
+print("Dushanba")
+print("Seshanba")
+print("Chorshanba")
