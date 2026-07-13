a956ac03b44497cbc51cabc22b86497d5d05b492
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-13 12:28:53 +0500

    Add: 13) Info blok (3 qatorli matn) [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/03-Birinchi_dastur_____print___va_kommentlar/13-13__Info_blok__3_qatorli_matn_.py
@@ -0,0 +1,7 @@
+# Quyidagi 3 qatorli matnni konsolga chiqaring:
+# Dastur haqida ma'lumot
+# Muallif: Aziz Academy
+# Versiya: 1.0
+print("Dastur haqida ma'lumot")
+print("Muallif: Aziz Academy")
+print("Versiya: 1.0")
