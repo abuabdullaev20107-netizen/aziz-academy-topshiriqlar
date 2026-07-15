69e38dd54cc7f60ca391dfcc565033a2bf09d354
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-15 11:43:12 +0500

    Add: 3) Uchta son yig'indisi [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/03-3__Uchta_son_yig_indisi.py
@@ -0,0 +1,5 @@
+# Bir qatorda uchta butun son kiriting (probel bilan).
+# Ularning yig'indisini hisoblab, "Yig'indi: <summa>" ko'rinishida chiqaring.
+a, b, c = input(). split()
+summa = int(a) + int(b) + int(c)
+print("Yig'indi:", summa)
