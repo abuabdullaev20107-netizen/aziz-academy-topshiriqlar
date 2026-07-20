8a6039dbaa46d51b5481e5e68b04d03ae596549f
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-20 12:12:53 +0500

    Add: 4) Uchta butun son o'rtachasi (float) [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/05-Sonlar__int_va_float___butun_va_kasr_sonlar/04-4__Uchta_butun_son_o_rtachasi__float_.py
@@ -0,0 +1,5 @@
+# Bir qatorda uchta butun son kiriting (probel bilan).
+# Ularning o'rtachasini (float) hisoblab, "Average: <natija>" ko'rinishida chiqaring.
+a, b, c = input(). split()
+ortacha = (int(a) + int(b) + int(c)) /3
+print(f"Average:", ortacha)
