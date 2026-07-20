8933be1dcaa01c9e1c453bb5ef23fe215ce3849d
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-20 12:05:57 +0500

    Add: 3) int va float yig'indisi [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/05-Sonlar__int_va_float___butun_va_kasr_sonlar/03-3__int_va_float_yig_indisi.py
@@ -0,0 +1,6 @@
+# 1-qator: butun son (int)
+# 2-qator: haqiqiy son (float)
+# Ularning yig'indisini hisoblab, "Sum: <natija>" ko'rinishida chiqaring.
+a = int(input())
+b = float(input())
+print(f"Sum:", a + b)
