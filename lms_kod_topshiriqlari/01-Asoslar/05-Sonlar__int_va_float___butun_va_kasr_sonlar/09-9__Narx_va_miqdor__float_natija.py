5802b699cff09c3fb18c860c4682738daf0ba7a7
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-22 11:05:31 +0500

    Add: 9) Narx va miqdor: float natija [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/05-Sonlar__int_va_float___butun_va_kasr_sonlar/09-9__Narx_va_miqdor__float_natija.py
@@ -0,0 +1,7 @@
+# 1-qator: bitta dona narxi (float)
+# 2-qator: miqdor (int)
+# Jami = narx * miqdor
+# "Total: <natija>" ko'rinishida chiqaring.
+narx = float(input())
+miqdor = int(input())
+print("Total:", narx * miqdor)
