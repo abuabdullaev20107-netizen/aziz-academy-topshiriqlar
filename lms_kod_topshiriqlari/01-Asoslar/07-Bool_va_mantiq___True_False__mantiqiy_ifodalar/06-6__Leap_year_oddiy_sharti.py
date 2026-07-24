991aa90cd3a0b3d2bbf5d91ffc7310dfa9fe8b42
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-24 11:26:43 +0500

    Add: 6) Leap year oddiy sharti [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/06-6__Leap_year_oddiy_sharti.py
@@ -0,0 +1,5 @@
+
+# Leap year sharti: (yil % 4 == 0 va yil % 100 != 0) yoki (yil % 400 == 0)
+# Ushbu shart natijasini (True/False) chiqaring.
+yil = int(input())
+print((yil % 4 == 0 and yil % 100 != 0) or (yil % 400 == 0))
