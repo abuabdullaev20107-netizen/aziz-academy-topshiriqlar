83457d087665560729a9b4441ecb3a2949ef723d
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-27 12:01:37 +0500

    Add: 7) To‘g‘ri to‘rtburchak perimetri [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/07-7__To_g_ri_to_rtburchak_perimetri.py
@@ -0,0 +1,7 @@
+# 1-qator: width (float)
+# 2-qator: height (float)
+# Perimetr: 2 * (width + height)
+# "Perimeter: <p>" ko'rinishida, 1 kasr bilan chiqaring.
+a = float(input())
+b = float(input())
+print("Perimeter:", (a + b) * 2)
