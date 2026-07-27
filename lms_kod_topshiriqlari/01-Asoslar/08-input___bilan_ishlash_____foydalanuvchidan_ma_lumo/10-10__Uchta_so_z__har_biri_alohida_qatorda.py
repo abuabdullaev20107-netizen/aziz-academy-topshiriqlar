0537eda4adc04b31084a227576b41038eb5001be
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-27 12:20:42 +0500

    Add: 10) Uchta so‘z: har biri alohida qatorda [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/10-10__Uchta_so_z__har_biri_alohida_qatorda.py
@@ -0,0 +1,9 @@
+# Bitta qatorda 3 ta so'z kiriting (bo'shliq bilan ajratilgan).
+# Ularni alohida qatorda chiqarib bering.
+# 1-qator: birinchi so'z
+# 2-qator: ikkinchi so'z
+# 3-qator: uchinchi so'z
+words = input().split()
+print(words[0])
+print(words[1])
+print(words[2])
