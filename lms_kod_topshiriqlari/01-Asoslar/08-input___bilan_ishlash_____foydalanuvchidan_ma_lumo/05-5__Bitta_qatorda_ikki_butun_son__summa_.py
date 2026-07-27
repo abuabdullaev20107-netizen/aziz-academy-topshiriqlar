e41ae6084904eb24b9e8fef3a2bc16e76b66a92a
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-27 11:43:30 +0500

    Add: 5) Bitta qatorda ikki butun son (summa) [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/05-5__Bitta_qatorda_ikki_butun_son__summa_.py
@@ -0,0 +1,5 @@
+# Bitta qatorda 2 ta butun son kiriting: a va b.
+# Ularni int ga o'tkazib, yig'indisini hisoblang.
+# "Sum: <natija>" ko'rinishida chiqaring.
+a, b = map(int, input().split())
+print("Sum:", a + b)
