3c8f1bf2ff74c1bc4c101506defa71a26a17a4eb
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-27 12:12:54 +0500

    Add: 9) So‘z va n: takrorlash [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/09-9__So_z_va_n__takrorlash.py
@@ -0,0 +1,8 @@
+# 1-qator: so'z
+# 2-qator: n (butun son)
+# So'zni n marta bo'shliq bilan ajratib chiqarib bering.
+# Masalan: word=hi, n=3 -> "hi hi hi"
+soz = input()
+son = int(input())
+soz1 = soz + ' '
+print(soz1 * son)
