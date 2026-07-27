7365754a4a5e73e94c3d10fb7462a286113a6c84
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-27 11:35:51 +0500

    Add: 4) Yosh + 1 [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/04-4__Yosh___1.py
@@ -0,0 +1,5 @@
+# Yoshni butun son sifatida kiriting.
+# Keyingi yildagi yoshni hisoblang: age + 1
+# "Next year: <yangi_yosh>" ko'rinishida chiqaring.
+age = int(input())
+print("Next year:", age + 1)
