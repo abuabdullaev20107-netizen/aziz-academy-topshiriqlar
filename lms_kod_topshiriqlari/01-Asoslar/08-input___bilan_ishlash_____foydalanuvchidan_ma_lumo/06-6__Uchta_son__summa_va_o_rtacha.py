f855de647bf05818841b994e9d11699843e58193
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-27 11:59:47 +0500

    Add: 6) Uchta son: summa va o‘rtacha [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/06-6__Uchta_son__summa_va_o_rtacha.py
@@ -0,0 +1,9 @@
+# 3 ta butun sonni alohida qatorlarda kiriting.
+# Ularning yig'indisi va o'rtachasini hisoblang.
+# 1-qator: "Sum: <yig'indi>"
+# 2-qator: "Avg: <o'rtacha>" (2 kasr bilan)
+a = int(input())
+b = int(input())
+c = int(input())
+print("Sum:", a + b + c)
+print("Avg:", f"{(a + b + c) / 3:.2f}")
