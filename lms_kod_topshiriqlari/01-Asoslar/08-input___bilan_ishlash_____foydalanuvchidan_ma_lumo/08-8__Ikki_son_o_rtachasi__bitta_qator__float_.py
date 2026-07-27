c394159a92f251fe87a7e32a63a429bb950f564a
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-27 12:06:44 +0500

    Add: 8) Ikki son o‘rtachasi (bitta qator, float) [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/08-8__Ikki_son_o_rtachasi__bitta_qator__float_.py
@@ -0,0 +1,4 @@
+# Bitta qatorda 2 ta son (float) kiriting.
+# O'rtachasini hisoblab, "Average: <natija>" ko'rinishida 2 kasr bilan chiqaring.
+a, b = map(float, input().split())
+print("Average:", f"{(a + b) / 2:.2f}")
