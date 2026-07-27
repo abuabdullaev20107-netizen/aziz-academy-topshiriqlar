4a1728f61da546b6d93454b781f773926c78cdbf
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-27 11:34:04 +0500

    Add: 3) Ism va familiya (ikki qator) [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/03-3__Ism_va_familiya__ikki_qator_.py
@@ -0,0 +1,6 @@
+# 1-qator: ism
+# 2-qator: familiya
+# "Familiya: <familiya>, Ism: <ism>" ko'rinishida chiqaring.
+ism = input()
+familiya = input()
+print("Familiya: " + familiya + "," + " Ism: " + ism)
