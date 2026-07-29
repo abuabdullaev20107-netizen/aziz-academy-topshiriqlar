77f985e7d19a913d404a149a1d041a2481f45fd6
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-29 08:56:27 +0500

    Add: 14) Narx va dona: jami summa [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/14-14__Narx_va_dona__jami_summa.py
@@ -0,0 +1,8 @@
+# 1-qator: narx (float)
+# 2-qator: dona (int)
+# Jami summa: narx * dona
+# "Total: <summa>" ko'rinishida 2 kasr bilan chiqaring.
+narx = float(input())
+dona = int(input())
+summa = narx * dona
+print("Total:", f"{summa:.2f}")
