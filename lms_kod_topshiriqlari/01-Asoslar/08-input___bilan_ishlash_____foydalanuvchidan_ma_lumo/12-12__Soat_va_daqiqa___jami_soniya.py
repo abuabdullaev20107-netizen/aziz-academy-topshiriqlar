94b0c15f3f15fa860452ed6b2a981801471b4b9d
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-29 08:37:05 +0500

    Add: 12) Soat va daqiqa → jami soniya [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/08-input___bilan_ishlash_____foydalanuvchidan_ma_lumo/12-12__Soat_va_daqiqa___jami_soniya.py
@@ -0,0 +1,7 @@
+# 1-qator: soat (int)
+# 2-qator: daqiqa (int)
+# Jami soniya: soat*3600 + daqiqa*60
+# "Seconds: <natija>" ko'rinishida chiqaring.
+soat = int(input())
+daqiqa = int(input())
+print("Seconds:", soat * 3600 + daqiqa * 60)
