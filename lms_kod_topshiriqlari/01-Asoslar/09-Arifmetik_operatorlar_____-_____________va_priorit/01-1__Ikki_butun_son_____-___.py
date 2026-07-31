1e4bb72ab10c6c2e5ab1f9d46abd7f709603a9ee
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-31 10:49:55 +0500

    Add: 1) Ikki butun son: +, -, * [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/09-Arifmetik_operatorlar_____-_____________va_priorit/01-1__Ikki_butun_son_____-___.py
@@ -0,0 +1,8 @@
+# a va b (bitta qatorda)
+# 1-qator: "Add: <a+b>"
+# 2-qator: "Sub: <a-b>"
+# 3-qator: "Mul: <a*b>"
+a, b = map(int,input().split())
+print("Add:", a + b)
+print("Sub:", a - b)
+print("Mul:", a * b)
