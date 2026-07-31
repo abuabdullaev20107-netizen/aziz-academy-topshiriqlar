8522b3603e9a108e53c6f42aeb5f183f7a6ff5a4
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-07-31 11:11:28 +0500

    Add: 10) 2 xonali son: o‘nlik va birlik (// va %) [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/09-Arifmetik_operatorlar_____-_____________va_priorit/10-10__2_xonali_son__o_nlik_va_birlik_____va___.py
@@ -0,0 +1,10 @@
+# n (10..99)
+# tens = n // 10
+# ones = n % 10
+# 1-qator: "Tens: <tens>"
+# 2-qator: "Ones: <ones>"
+n = int(input())
+tens = n // 10
+ones = n % 10
+print("Tens:", tens)
+print("Ones:", ones)
