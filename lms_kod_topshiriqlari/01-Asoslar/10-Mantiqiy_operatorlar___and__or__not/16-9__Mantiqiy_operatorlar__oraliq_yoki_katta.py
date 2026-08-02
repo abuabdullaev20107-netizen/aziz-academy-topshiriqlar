c5fe0193c3af0d3d14af9bfc50fb95dfccbff0c0
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-08-02 18:45:00 +0500

    Add: 9) Mantiqiy operatorlar: oraliq yoki katta [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/10-Mantiqiy_operatorlar___and__or__not/16-9__Mantiqiy_operatorlar__oraliq_yoki_katta.py
@@ -0,0 +1,5 @@
+# Bitta butun son kiriting.
+# True chiqaring, agar son 10–20 oralig'ida (kiritilgan) bo'lsa YOKI 90 dan katta bo'lsa.
+# Aks holda False chiqaring.
+a = int(input())
+print(( a >= 10 and a <= 20) or a > 90)
