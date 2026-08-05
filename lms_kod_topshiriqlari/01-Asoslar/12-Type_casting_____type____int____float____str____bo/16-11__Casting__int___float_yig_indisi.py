3f4829654dc51f2ae07e6b1cabd395002ff37f69
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-08-05 11:38:31 +0500

    Add: 11) Casting: int + float yig'indisi [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/12-Type_casting_____type____int____float____str____bo/16-11__Casting__int___float_yig_indisi.py
@@ -0,0 +1,4 @@
+# Bir qatorda ikkita qiymat kiriting: butun va haqiqiy (masalan: 10 2.5).
+# Ularni int va float ga aylantirib, yig'indisini chiqaring.
+a, b = input().split()
+print(float(a) + float(b))
