3a856a3601247e4ae798f18956d8cc9da6774b82
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-08-05 12:05:42 +0500

    Add: 12) Oddiy kalkulyator (+, -, *, /) [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-12__Oddiy_kalkulyator_____-_______.py
@@ -0,0 +1,14 @@
+# Ikki butun son kiriting.
+# Birinchi qatorda a va b: masalan 8 2
+# Siz quyidagi to'rtta amal natijasini chiqaring (har biri alohida qatorda):
+# a + b
+# a - b
+# a * b
+# a / b
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
