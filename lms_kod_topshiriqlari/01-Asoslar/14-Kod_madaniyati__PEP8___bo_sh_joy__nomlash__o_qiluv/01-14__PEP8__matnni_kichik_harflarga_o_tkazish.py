8eed1c50c2e862e4abade0633b22cd20b1f69992
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-08-07 11:30:26 +0500

    Add: 14) PEP8: matnni kichik harflarga o'tkazish [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/01-Asoslar/14-Kod_madaniyati__PEP8___bo_sh_joy__nomlash__o_qiluv/01-14__PEP8__matnni_kichik_harflarga_o_tkazish.py
@@ -0,0 +1,5 @@
+# Bitta satr matn kiriting.
+# Uni kichik harflarga o'tkazib chiqaring.
+# Izoh: o'zgaruvchi nomlari PEP8 bo'yicha snake_case bo'lsin (masalan: user_text).
+matn = input().lower()
+print(matn)
