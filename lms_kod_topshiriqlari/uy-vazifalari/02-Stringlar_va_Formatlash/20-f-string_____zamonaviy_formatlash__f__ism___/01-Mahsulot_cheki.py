356224e5aba01ffc4793104c2a8f79bb6d408c00
abuabdullaev20107-netizen <abuabdullaev20107@gmail.com>
2026-08-26 12:05:49 +0500

    Add: Mahsulot cheki [abuabdullaev20107-netizen]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/20-f-string_____zamonaviy_formatlash__f__ism___/01-Mahsulot_cheki.py
@@ -0,0 +1,8 @@
+nom = input()
+narx = int(input())
+soni = int(input())
+print("Mahsulot:", nom)
+print(f"Narxi: {narx} som")
+print("Soni:", soni)
+natija = soni * narx
+print(f"Jami: {natija} som")
